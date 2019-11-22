7bbcec467b57a87af5a35bb83a33eae15c109c7e
fatfirebird <fatfirebird@gmail.com>
2019-11-22 23:15:58 +0500

    add styles for input range

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -14,11 +14,91 @@ const Label = styled.label`
   margin-bottom: 15px;
 `
 
+const Range = styled.input`
+  & {
+    -webkit-appearance: none;
+    width: 100%;
+    margin: 10px 0;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 5px;
+    cursor: pointer;
+    background: #939393;
+  }
+  &::-webkit-slider-thumb {
+    border: 4px solid #939393;
+    height: 25px;
+    width: 25px;
+    border-radius: 50px;
+    background: #ffffff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -10px;
+  }
+  &:focus::-webkit-slider-runnable-track {
+    background: #9b9b9b;
+  }
+  &::-moz-range-track {
+    width: 100%;
+    height: 5px;
+    cursor: pointer;
+    background: #939393;
+  }
+  &::-moz-range-thumb {
+    border: 4px solid #939393;
+    height: 25px;
+    width: 25px;
+    border-radius: 50px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  &::-ms-track {
+    width: 100%;
+    height: 5px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  &::-ms-fill-lower {
+    background: #8b8b8b;
+    border: 0px solid rgba(0, 0, 1, 0);
+    border-radius: 0px;
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0), 0px 0px 0px rgba(13, 13, 13, 0);
+  }
+  &::-ms-fill-upper {
+    background: #939393;
+    border: 0px solid rgba(0, 0, 1, 0);
+    border-radius: 0px;
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0), 0px 0px 0px rgba(13, 13, 13, 0);
+  }
+  &::-ms-thumb {
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0), 0px 0px 0px rgba(13, 13, 13, 0);
+    border: 4px solid #939393;
+    height: 25px;
+    width: 25px;
+    border-radius: 50px;
+    background: #ffffff;
+    cursor: pointer;
+    height: 5px;
+  }
+  &:focus::-ms-fill-lower {
+    background: #939393;
+  }
+  &:focus::-ms-fill-upper {
+    background: #9b9b9b;
+  }
+`
+
 const Filter = () => {
   return(
     <FilterWrapper>
       <Label>Фильтрнейм</Label>
-      <input type = 'range' min = '0' max = '1' step = '0.1' defaultValue = '0'/>
+      <Range type = 'range' min = '0' max = '1' step = '0.1' defaultValue = '0'/>
     </FilterWrapper>
   )
 }
